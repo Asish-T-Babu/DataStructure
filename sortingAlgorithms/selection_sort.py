9d95ea85090bde8901e12c32e4dccb84ef3448cb
Asish-T-Babu <asishtbabu@gmail.com>
2024-02-26 22:04:00 +0530

    Updated Selection Sort

--- sortingAlgorithms/selection_sort.py
+++ sortingAlgorithms/selection_sort.py
@@ -1,7 +1,7 @@
 unsorted_list = [6,7,8,5,4]
 for i in range(len(unsorted_list)-1):
     min = i
-    for j in range (i,len(unsorted_list)):
+    for j in range (i+1, len(unsorted_list)):
         if unsorted_list[i]>unsorted_list[j]:
             min = j
     if min != i:
