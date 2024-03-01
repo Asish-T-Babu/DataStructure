4096d8a013f442aa9bbe480cc18c7d9fbd542594
Asish-T-Babu <asishtbabu@gmail.com>
2024-03-01 22:04:41 +0530

    Queue

---
+++ queue/queue.py
@@ -0,0 +1,14 @@
+queue = []
+
+# FIFO - first in first out
+
+# Enqueue
+queue.append(1)
+queue.append(3)
+queue.append(2)
+print(queue)
+
+#Dequeue
+print(queue.pop(0))
+print(queue.pop(0))
+print(queue)
